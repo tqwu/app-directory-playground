69be681551eb53dd6ad4221c2382cb9e059d013f
Teresa <teresa.me1210@gmail.com>
2023-08-07 02:01:03 -0700

    Modifying app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <div className="space-y-8">
-      <h1 className="text-xl font-medium text-gray-300">Examples</h1>
+      <h1 className="text-xl font-medium text-gray-300">
+        Hi there this is Teresa!
+      </h1>
 
       <div className="space-y-10 text-white">
         {demos.map((section) => {
